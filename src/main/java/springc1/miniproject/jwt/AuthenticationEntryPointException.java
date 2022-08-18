58d648ba2ff8317b7ea769298bc7d8e55250be67
ctr0812 <andy0812@naver.com>
2022-08-18 11:31:12 +0900

    깃헙 액션 확인 커밋

--- src/main/java/springc1/miniproject/jwt/AuthenticationEntryPointException.java
+++ src/main/java/springc1/miniproject/jwt/AuthenticationEntryPointException.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+
+
 @Component
 public class AuthenticationEntryPointException implements
         AuthenticationEntryPoint {
@@ -19,7 +22,7 @@ public class AuthenticationEntryPointException implements
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response,
                        AuthenticationException authException) throws IOException {
-    String result = objectMapper.writeValueAsString(ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다."));
+    String result = objectMapper.writeValueAsString(ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다~"));
     response.setContentType("application/json");
     response.setCharacterEncoding("utf-8");
     response.getWriter().write(result);
